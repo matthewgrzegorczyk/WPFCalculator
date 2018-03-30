6a0e2fc17e9d4fdae96c64b45e82c8350650c281
Matthew Grzegorczyk <matthewgrzegorczyk@gmail.com>
2018-03-30 20:09:26 +0200

    Basic calculator setup

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -23,6 +23,51 @@ namespace Calculator
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            int min_width = 250;
+            this.MinWidth = this.Width > min_width ? this.Width : min_width;
+            this.MinHeight = this.Height;
+        }
+
+        private void OnCalculate(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int a = int.Parse(first_number.Text);
+                int b = int.Parse(second_number.Text);
+                string op = operation.Text;
+
+                switch (op)
+                {
+                    case "+":
+                        output.Text = (a + b).ToString();
+                        break;
+                    case "-":
+                        output.Text = (a - b).ToString();
+                        break;
+                    case "/":
+                        output.Text = (a / b).ToString();
+                        break;
+                    case "*":
+                        output.Text = (a * b).ToString();
+                        break;
+                    default:
+                        MessageBox.Show("Houston mamy problem: Nieprawidłowy operator.");
+                        break;
+                }
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Dżejms Błont się wkradł.");
+            }
+            catch (DivideByZeroException ex)
+            {
+                MessageBox.Show("Dżejms Błont nie pozwala dzielić przez 0.");
+            }
         }
     }
 }
